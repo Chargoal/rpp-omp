167750624c5cd26f6f364a8f2dbe8452f5fa8975
Evgeniy <chargoalup@gmail.com>
2024-04-19 02:37:55 +0300

    WORK

--- ompSort.cpp
+++ ompSort.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
-#include <algorithm> // For std::merge
+#include <algorithm>
 #include <omp.h>
 
 using namespace std;
@@ -10,6 +10,7 @@ using namespace std::chrono;
 
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
+    #pragma omp parallel for
     for (int i = 0; i < n - 1; ++i) {
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
@@ -21,14 +22,15 @@ void bubbleSort(vector<int>& arr) {
 
 int main(int argc, char** argv) {
     if (argc != 3) {
-        cout << "Usage: ./ompSort <number of processes> <input file name>\n";
+        cout << "Usage: ./ompSort.exe <number of threads> <input file name>\n";
         return 1;
     }
 
-    int numProcesses = atoi(argv[1]);
-    string filename = argv[2];
+    int numThreads = atoi(argv[1]);
+    omp_set_num_threads(numThreads);
 
-    auto start = chrono::high_resolution_clock::now();
+    string filename = argv[2];
+    auto start = high_resolution_clock::now();
     ifstream inputFile(filename);
     if (!inputFile) {
         cout << "Error: Unable to open file " << filename << endl;
@@ -37,47 +39,25 @@ int main(int argc, char** argv) {
 
     vector<int> numbers;
     int number;
-
     while (inputFile >> number) {
         numbers.push_back(number);
     }
-
     inputFile.close();
 
-    int numbersSize = numbers.size();
-    if (numbersSize == 0) {
-        cout << "Error: No data found in file " << filename << endl;
-        return 1;
-    }
+    // Sort the numbers using OpenMP
+    bubbleSort(numbers);
 
-    // Divide data into chunks for each process
-    int chunkSize = numbersSize / numProcesses;
-    #pragma omp parallel num_threads(numProcesses)
-    {
-        int tid = omp_get_thread_num();
-        int startIdx = tid * chunkSize;
-        int endIdx = (tid == numProcesses - 1) ? numbersSize : (tid + 1) * chunkSize;
-        vector<int> localData(numbers.begin() + startIdx, numbers.begin() + endIdx);
-        bubbleSort(localData);
-        #pragma omp critical
-        numbers.insert(numbers.begin() + startIdx, localData.begin(), localData.end());
-    }
-
-    // Merge sorted chunks
-    for (int i = 1; i < numProcesses; ++i) {
-        int startIndex = i * chunkSize;
-        merge(numbers.begin(), numbers.begin() + startIndex, numbers.begin() + startIndex,
-            numbers.begin() + startIndex + chunkSize, numbers.begin());
-    }
-
+    // Stop timer
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
 
     cout << "Sorting duration: " << duration.count() << " milliseconds" << endl;
 
+    // Output sorted array to file
     ofstream outputFile("output.txt");
     if (!outputFile) {
         cout << "Error: Unable to create output file." << endl;
+        return 1;
     } else {
         for (int num : numbers) {
             outputFile << num << " ";
