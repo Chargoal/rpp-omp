3d34f300601cea287b036f54177d5d87df7606d9
Evgeniy <chargoalup@gmail.com>
2024-04-19 04:22:35 +0300

    DONE

--- ompSort.cpp
+++ ompSort.cpp
@@ -1,69 +1,115 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <chrono>
-#include <algorithm>
 #include <omp.h>
+#include <chrono>
 
 using namespace std;
-using namespace std::chrono;
+using namespace chrono;
 
+// Function for bubble sort
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
-    #pragma omp parallel for
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n - i - 1; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = 0; j < n-i-1; ++j) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
             }
         }
     }
 }
 
-int main(int argc, char** argv) {
+// Function to merge two sorted arrays
+vector<int> merge(const vector<int>& left, const vector<int>& right) {
+    vector<int> merged;
+    size_t leftIndex = 0, rightIndex = 0;
+
+    while (leftIndex < left.size() && rightIndex < right.size()) {
+        if (left[leftIndex] <= right[rightIndex]) {
+            merged.push_back(left[leftIndex]);
+            ++leftIndex;
+        } else {
+            merged.push_back(right[rightIndex]);
+            ++rightIndex;
+        }
+    }
+
+    while (leftIndex < left.size()) {
+        merged.push_back(left[leftIndex]);
+        ++leftIndex;
+    }
+
+    while (rightIndex < right.size()) {
+        merged.push_back(right[rightIndex]);
+        ++rightIndex;
+    }
+
+    return merged;
+}
+
+int main(int argc, char* argv[]) {
     if (argc != 3) {
-        cout << "Usage: ./ompSort.exe <number of threads> <input file name>\n";
+        cerr << "Usage: " << argv[0] << " <number of threads> <input file name>\n";
         return 1;
     }
 
     int numThreads = atoi(argv[1]);
-    omp_set_num_threads(numThreads);
+    string inputFile = argv[2];
 
-    string filename = argv[2];
-    auto start = high_resolution_clock::now();
-    ifstream inputFile(filename);
-    if (!inputFile) {
-        cout << "Error: Unable to open file " << filename << endl;
+    // Reading array from file
+    ifstream inFile(inputFile);
+    if (!inFile) {
+        cerr << "Error: Unable to open input file: " << inputFile << endl;
         return 1;
     }
 
-    vector<int> numbers;
-    int number;
-    while (inputFile >> number) {
-        numbers.push_back(number);
+    vector<int> data;
+    int value;
+    while (inFile >> value) {
+        data.push_back(value);
     }
-    inputFile.close();
+    inFile.close();
 
-    // Sort the numbers using OpenMP
-    bubbleSort(numbers);
+    // Measure start time
+    auto start = high_resolution_clock::now();
 
-    // Stop timer
+    // Distributing array among threads
+    size_t chunkSize = data.size() / numThreads;
+    vector<vector<int>> chunks(numThreads);
+    #pragma omp parallel num_threads(numThreads)
+    {
+        int threadID = omp_get_thread_num();
+        size_t startIndex = threadID * chunkSize;
+        size_t endIndex = (threadID == numThreads - 1) ? data.size() : startIndex + chunkSize;
+        chunks[threadID].assign(data.begin() + startIndex, data.begin() + endIndex);
+
+        // Sorting its chunk of array
+        bubbleSort(chunks[threadID]);
+    }
+
+    // Merging sorted chunks
+    vector<int> result = chunks[0];
+    for (int i = 1; i < numThreads; ++i) {
+        result = merge(result, chunks[i]);
+    }
+
+    // Measure end time
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
 
-    cout << "Sorting duration: " << duration.count() << " milliseconds" << endl;
-
-    // Output sorted array to file
-    ofstream outputFile("output.txt");
-    if (!outputFile) {
-        cout << "Error: Unable to create output file." << endl;
+    // Writing sorted array to file
+    ofstream outFile("output.txt");
+    if (!outFile) {
+        cerr << "Error: Unable to open output file: output.txt\n";
         return 1;
-    } else {
-        for (int num : numbers) {
-            outputFile << num << " ";
-        }
-        outputFile.close();
     }
+
+    for (const auto& num : result) {
+        outFile << num << " ";
+    }
+    outFile.close();
+
+    cout << "Time taken: " << duration.count() << " milliseconds" << endl;
 
     return 0;
 }
